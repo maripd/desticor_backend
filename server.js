41daeeca9c22e2cbd0366384555cbd52b8e09a7d
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2023-04-16 20:21:01 -0700

    added login

--- server.js
+++ server.js
@@ -1,54 +1,99 @@
-const express = require('express')
-const routes = require('./routes/index.js')
-const db = require('./db')
-const logger = require('morgan')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const bcrypt = require('bcrypt')
-const user = require('./models/user.js')
+const express = require("express");
+const routes = require("./routes/index.js");
+const db = require("./db");
+const logger = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcryptjs");
+const user = require("./models/user.js");
 
 // const jsonParser = bodyParser.json()
 // const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
-const app = express()
-app.use(express.json())
-app.use(logger('dev'))
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(logger("dev"));
+app.use(cors());
 // app.use(express.urlencoded({}))
-app.use(express.static(`${__dirname}/client/build`))
-app.use('/', routes)
-app.get('/*', (req, res) => {
-  res.sendFile(`${__dirname}/client/build/index.html`)
-})
-app.use('/createuser', user)
-const hashPassword = (req, res, next) => {
+app.use(express.static(`${__dirname}/client/build`));
+app.use("/", routes);
+app.get("/*", (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`);
+});
+
+// register endpoint
+app.post("/createuser", (req, res) => {
+  // hash the password
   bcrypt
     .hash(req.body.password, 10)
-    .then((hash) => {
-      req.body.password = hash
-      next()
-    })
-    .catch((err) => {
-      console.error('Error hashing password:', err)
-      res.status(500).send('Password was not hashed successfully')
-    })
-}
+    .then((hashedPassword) => {
+      // create a new user instance and collect the data
+      const user = new User({
+        emailAddress: request.body.emailAddress,
+        password: hashedPassword
+      });
 
-app.post('/createuser', hashPassword, (req, res) => {
-  const { email, password } = req.body
-
-  const newUser = new user({ email, password })
-  newUser
-    .save()
-    .then(() => {
-      res.status(201).send('User created successfully')
+      // save the new user
+      user
+        .save()
+        // return success if the new user is added to the database successfully
+        .then((result) => {
+          res.status(201).send({
+            message: "User Created Successfully",
+            result
+          });
+        })
+        // catch error if the new user wasn't added successfully to the database
+        .catch((error) => {
+          res.status(500).send({
+            message: "Error creating user",
+            error
+          });
+        });
     })
-    .catch((err) => {
-      console.error('Error creating user:', err)
-      res.status(500).send('Internal server error')
-    })
-})
+    // catch error if the password hash isn't successful
+    .catch((e) => {
+      res.status(500).send({
+        message: "Password was not hashed successfully",
+        e
+      });
+    });
+});
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+// login endpoint
+app.post("/login", (req, res) => {
+  const { emailAddress, password } = req.body;
+
+  // find the user by email
+  user.findOne({ emailAddress }, (err, user) => {
+    if (err) {
+      // return error if there's a database error
+      return res.status(500).json({ error: err.message });
+    }
+
+    if (!user) {
+      // return error if user not found
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // check the password
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (err) {
+        // return error if there's a password comparison error
+        return res.status(500).json({ error: err.message });
+      }
+
+      if (!result) {
+        // return error if password is incorrect
+        return res.status(401).json({ message: "Incorrect password" });
+      }
+
+      // return success if email and password are correct
+      res.status(200).json({ message: "Login successful", user });
+    });
+  });
+});
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
